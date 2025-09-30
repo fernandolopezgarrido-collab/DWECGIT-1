395a0c491e0e914d000a0fa51134234631605f28
fernandolopezgarrido-collab <fernando.lopez.garrido@iesjulianmarias.es>
2025-09-30 08:23:38 +0200

    Otra prueba del git

---
+++ funcionGlobalObjectDate.js
@@ -0,0 +1,64 @@
+//Fecha y hora actual
+let fechaActual = new Date();
+console.log("Fecha actual:", fechaActual);
+
+//Desde milisegundos (desde 1 enero 1970)
+let fechaMilis = new Date(1759156720831); // fecha de hoy sacada con date.now
+console.log("Fecha desde milisegundos:", fechaMilis);
+
+//Desde cadena de texto
+let fechaTexto = new Date("2025-09-29");
+console.log("Fecha desde texto:", fechaTexto);
+
+//Desde componentes individuales
+let fechaCompuesta = new Date(2025, 8, 29, 16, 30, 0, 0); 
+console.log("Fecha compuesta:", fechaCompuesta);
+
+//Devuelve un valor numerico correspondiente a la fecha y hora actual
+console.log("Prueba now");
+console.log(Date.now());//1759156720831
+
+//Devuelve un valor numerico correspondiente a la fecha y hora actual
+console.log("Prueba parse");
+console.log(Date.parse("2025-09-29"));//1759156720831
+
+//Devuelve el año de la fecha
+console.log("Prueba getFullYear:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getFullYear());//2025
+
+//Devuelve el mes de la fecha
+console.log("Prueba getMonth:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getMonth());//8
+
+//Devuelve el dia del mes de la fecha
+console.log("Prueba getDate:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getDate());//29
+
+//Devuelve el dia del mes de la fecha
+console.log("Prueba getDate:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getDay());//1 (0 domingo)
+
+//Devuelve hora,min,segundos,milisegundos
+console.log("Prueba hora,min,segundos,milisegundos:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getHours());//16
+console.log(fechaCompuesta.getMinutes());//30
+console.log(fechaCompuesta.getSeconds());//0
+console.log(fechaCompuesta.getMilliseconds());//0
+
+//Devuelve el valor numerico en ms correspondiente a la fecha
+console.log("Prueba getTime:");
+console.log(fechaCompuesta);//Mon Sep 29 2025 16:30:00 GMT+0200 (hora de verano de Europa central)
+console.log(fechaCompuesta.getTime());//1759156200000
+
+//Modifican la fecha
+console.log("Prueba sets:")
+console.log("Fecha anterior  :",fechaCompuesta);
+fechaCompuesta.setFullYear(2030);
+fechaCompuesta.setMonth(0); 
+fechaCompuesta.setDate(1);
+console.log("Fecha modificada:", fechaCompuesta);
--- funcionMath.js
+++ funcionMath.js
@@ -1,20 +1,24 @@
 let numero1 = 22;
-let numero2= 1;
+let numero2 = 1;
 let numero3 = -10;
-let numero4= 4;
-let anguloGrados= 30;
+let numero4 = 4;
+let numero5 = 10;
+let numero6 = 5.67;
+let numero7 = 5.3;
+let numeros=[1,-6,-77,88];
+let anguloGrados = 30;
 let anguloRadianes = anguloGrados * (Math.PI / 180);
 
 //Propiedad del numero Pi:
 console.log("Prueba PI:")
 console.log(Math.PI);//3.141592653589793
-console.log(Math.PI+numero2);//4.141592653589793
+console.log(Math.PI + numero2);//4.141592653589793
 
 //Propiedad con el numero E:
 console.log("Prueba E:")
 console.log(Math.E);//2.718281828459045
-console.log(Math.E+numero2);//3.718281828459045
-//
+console.log(Math.E + numero2);//3.718281828459045
+
 //Valor absoluto de un numero:
 console.log("Prueba valor absoluto:")
 console.log(numero3);//10
@@ -33,7 +37,7 @@ console.log(Math.cos(anguloRadianes));//0.8660254037844387
 //Tangente de un angulo (hay que pasarlo en radianes)
 console.log("Prueba tangente:")
 console.log(anguloGrados);//30
-console.log(Math.tan(anguloRadianes));0.5773502691896257
+console.log(Math.tan(anguloRadianes)); 0.5773502691896257
 
 //Exponencia e al numero indicado en el parametro
 console.log("Prueba exponente:")
@@ -42,7 +46,60 @@ console.log(Math.E);//2.718281828459045
 console.log(Math.exp(numero4));//54.598150033144236
 
 //Exponencia e al numero indicado en el parametro
-console.log("Prueba logaritmo:")
+console.log("Prueba logaritmo neperiano:")
 console.log(numero4);//4
-console.log(Math.E);//2.718281828459045
-console.log(Math.exp(numero4));//54.598150033144236
+console.log(numero5);//10
+console.log(Math.log(numero4));//1.3862943611198906
+console.log("Prueba logaritmo en base 10:")//Cambiar el dividendo a la base del logaritmo deseado
+console.log(Math.log(numero4)/Math.log(numero5));//0.6020599913279623
+
+//Redondea hacia arriba
+console.log("Prueba ceil:")
+console.log(numero6);//5.67
+console.log(Math.ceil(numero6));//6
+
+//Redondea hacia arriba
+console.log("Prueba floor:")
+console.log(numero6);//5.67
+console.log(Math.floor(numero6));//5
+
+//Redondea hacia la parte entera mas proxima
+console.log("Prueba round:")
+console.log(numero6);//5.67
+console.log(numero7);//5.3
+console.log(Math.round(numero6));//6
+console.log(Math.round(numero7));//5
+
+//Eleva el primer argumento al segundo
+console.log("Prueba pow:")
+console.log(numero4);//4
+console.log(Math.pow(numero4,numero4));//256
+
+//Devuelve el menor de sus argumentos
+console.log("Prueba min:")
+console.log(numero1);//22
+console.log(numero2);//1
+console.log(numero3);//-10
+console.log(numero4);//4
+console.log(numero5);//5.67
+console.log(Math.min(numero1,numero2,numero3,numero4,numero5,numero6));//-10
+
+//Devuelve el mayor de sus argumentos
+console.log("Prueba max:")
+console.log(numero1);//22
+console.log(numero2);//1
+console.log(numero3);//-10
+console.log(numero4);//4
+console.log(numero5);//5.67
+console.log(Math.max(numero1,numero2,numero3,numero4,numero5,numero6));//22
+
+//Hace la raiz 
+console.log("Prueba raiz:")
+console.log(numero4);//4
+console.log(Math.sqrt(numero4));//2
+
+//Devuelve numero aleatorio entre los parametros
+console.log("Prueba random:")
+console.log(numero2);//1
+console.log(numero4);//4
+console.log(Math.round(Math.random(numero1,numero4)));//aleatorio redondeado al entero mas cercano
